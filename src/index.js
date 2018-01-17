5d7cb7a33b058cedcd020195ddae9e99c315ce3d
Nadeeth Lansakara <nadeeth.lansakara@solnet.co.nz>
2018-01-17 16:11:42 +1300

    Improvements to load as an NPM package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+export { Alert } from './components/Alert';
+export { Button } from './components/Button';
+export { Modal, ModalHeader, ModalBody, ModalFooter } from './components/Modal';
